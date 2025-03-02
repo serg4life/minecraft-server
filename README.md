9838895b4bdf547f26b6dbe85d1f56da7f406ad0
serg4life <sergimendi2012@gmail.com>
2025-03-02 13:13:23 +0100

    install.sh modified with parameters

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ wget https://piston-data.mojang.com/v1/objects/4707d00eb834b446575d89a61a11b5d54
 wget https://download.bell-sw.com/java/21.0.6+10/bellsoft-jdk21.0.6+10-linux-aarch64.deb
 
 sudo apt install ./bellsoft-jdk21.0.6+10-linux-aarch64.deb
+
+screen -S <session pid> -X quit
--- install.sh
+++ install.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
+
 echo "Installing Minecraft server files"
+echo "Downloading Oficial Minecraft Vanilla server"
+wget https://piston-data.mojang.com/v1/objects/4707d00eb834b446575d89a61a11b5d548d8c001/server.jar
+echo "Done."
+
+echo "Downloading Liberica JDK 21.0.6"
+wget https://download.bell-sw.com/java/21.0.6+10/bellsoft-jdk21.0.6+10-linux-aarch64.deb
+echo "Done."
+
+echo "Installing Liberica JDK 21.0.6"
+sudo apt install ./sudo apt install ./bellsoft-jdk21.0.6+10-linux-aarch64.deb
+rm ./bellsoft-jdk21.0.6+10-linux-aarch64.deb
+echo "Done."
+
+echo "Installing screen from the repositories"
+sudo apt install screen
+echo "Done."
+
+echo "Configuring the service"
+cp ./services/minecraft-server.service /lib/systemd/system/
+systemctl enable minecraft-server.service
+systemctl start minecraft-server.service
+systemctl status minecraft-server.sevice
+echo "Done."
+
 chmod a+x ./mcstart
 sudo install ./mcstart /usr/local/bin
 echo "Done."
